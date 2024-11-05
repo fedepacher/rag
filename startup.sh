bb73a16ee44360c4f8a5e00b589ff650148342f1
Char <charlyvare19@gmail.com>
2024-11-04 21:27:29 -0300

    Teting ollama in docker

---
+++ startup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# # Iniciar el servicio de Ollama en segundo plano
+# ollama serve &
+ollama serve
+# echo "Mistral Descargando.................................................................."
+# ollama pull mistral
+
+# # Esperar unos segundos para asegurar que Ollama esté en funcionamiento
+# sleep 5
+
+# # Ejecutar el modelo llama3
+#ollama run mistral
