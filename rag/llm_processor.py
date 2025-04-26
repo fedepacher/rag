95a592324c8f2d8be1a5bd1b763947387941cb59
fedepacher <fede_pacher@hotmail.com>
2025-04-26 16:04:56 -0300

    refactor: added try except to cath errors

--- rag/llm_processor.py
+++ rag/llm_processor.py
@@ -35,29 +35,30 @@ class LLMProcessorOpenAI(BaseLLMProcessor):
         super().__init__(llm, embedding, context_length)
 
     def ask_question(self, question: str, context):
-        logging.debug(f"Processing {len(context)} chunks of text for question")
-        vectorstore = Chroma.from_texts(texts=context, embedding=self.embedding)
-
-        template = """Eres un asistente que debe responder preguntas basadas únicamente en la información proporcionada en el siguiente documento. No inventes respuestas ni utilices conocimientos externos. Si la respuesta no se encuentra en el documento, simplemente responde: "No sé la respuesta basada en la información proporcionada."
-
-                    [DOCUMENTO: {context}]
-                    
-                    Pregunta: {question}
-                    Respuesta:
-                   """
-        qa_chain_prompt = PromptTemplate.from_template(template)  # Run chain
-        qachain = RetrievalQA.from_chain_type(
-            self.llm,
-            retriever=vectorstore.as_retriever(),
-            return_source_documents=False,
-            chain_type_kwargs={"prompt": qa_chain_prompt}
-        )
-        llm_response = ''
         try:
+            logging.debug(f"Processing {len(context)} chunks of text for question")
+            vectorstore = Chroma.from_texts(texts=context, embedding=self.embedding)
+
+            template = """Eres un asistente que debe responder preguntas basadas únicamente en la información proporcionada en el siguiente documento. No inventes respuestas ni utilices conocimientos externos. Si la respuesta no se encuentra en el documento, simplemente responde: "No sé la respuesta basada en la información proporcionada."
+    
+                        [DOCUMENTO: {context}]
+                        
+                        Pregunta: {question}
+                        Respuesta:
+                       """
+            qa_chain_prompt = PromptTemplate.from_template(template)  # Run chain
+            qachain = RetrievalQA.from_chain_type(
+                self.llm,
+                retriever=vectorstore.as_retriever(),
+                return_source_documents=False,
+                chain_type_kwargs={"prompt": qa_chain_prompt}
+            )
+            llm_response = ''
+
             logging.debug(f"Waiting answer for question")
             llm_response = qachain({"query": question})
         except Exception as err:
-            logging.info(f'Exception occurred on chunk: {err}')
+            logging.error(f'Exception occurred on chunk: {err}')
         return llm_response
 
 
@@ -66,25 +67,26 @@ class LLMProcessorOpenLLM(BaseLLMProcessor):
         super().__init__(llm, None, None)
 
     def ask_question(self, question: str, context):
-        template = """Eres un asistente que debe responder preguntas basadas únicamente en la información proporcionada en el siguiente documento. No inventes respuestas ni utilices conocimientos externos. Si la respuesta no se encuentra en el documento, simplemente responde: "No sé la respuesta basada en la información proporcionada."
-
-                    [DOCUMENTO: {context}]
-                    
-                    Pregunta: {question}
-                    Respuesta:
-                   """
-
-        qa_chain_prompt = PromptTemplate(template=template, input_variables=["question", "context"])
-
-        # Create the LLMChain
-        chain = LLMChain(llm=self.llm, prompt=qa_chain_prompt)
-
-        logging.debug(f"Waiting answer for question")
-        llm_response = ''
         try:
+            template = """Eres un asistente que debe responder preguntas basadas únicamente en la información proporcionada en el siguiente documento. No inventes respuestas ni utilices conocimientos externos. Si la respuesta no se encuentra en el documento, simplemente responde: "No sé la respuesta basada en la información proporcionada."
+    
+                        [DOCUMENTO: {context}]
+                        
+                        Pregunta: {question}
+                        Respuesta:
+                       """
+
+            qa_chain_prompt = PromptTemplate(template=template, input_variables=["question", "context"])
+
+            # Create the LLMChain
+            chain = LLMChain(llm=self.llm, prompt=qa_chain_prompt)
+
+            logging.debug(f"Waiting answer for question")
+            llm_response = ''
+
             llm_response: str = chain.run({"question": question, "context": context})
         except Exception as err:
-            logging.info(f'Exception occurred on text: {err}')
+            logging.error(f'Exception occurred on text: {err}')
 
         return llm_response
 
